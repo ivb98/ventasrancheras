73e7acd0a7e52581b8e2495cdcdf8315dd8f67ee
ivb98 <ivanvalette@hotmail.com>
2020-03-24 01:32:20 -0400

    fix: Fixed import casing

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const express = require("express");
-const AuthTokenRoutes = require("./erp/OAuth2/auth/authToken.routes");
+const AuthTokenRoutes = require("./erp/OAuth2/Auth/authToken.routes");
 const CachedToken = require("./erp/OAuth2/cache/tokenCache");
-const QBO = require("./erp/OAuth2/auth/QBOAuth");
+const QBO = require("./erp/OAuth2/Auth/QBOAuth");
 const { port } = require("./config/index");
 // const { getInventory } = require("./erp/Inventory/Inventory");
 const { getCustomers } = require("./erp/Customer/Customer");
