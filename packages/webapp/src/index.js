e3e11c06ef5356f9ae4d8b2d22ea5f93808717ea
arisolgo <arielsolano39@gmail.com>
2020-03-24 21:58:07 -0400

    login page mockup done

--- packages/webapp/src/index.js
+++ packages/webapp/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import 'bootstrap/dist/css/bootstrap.css';
 import './global.css'
+import Login from './pages/Login'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Login />, document.getElementById("root"));
--- packages/webapp/src/pages/Login.js
+++ packages/webapp/src/pages/Login.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import "../components/styles/Login.css"
 
 class Login extends React.Component {
     render(){
         return(
-           <h1>Login</h1> 
+        <div className="loginContent">
+           <h1 className="pageTitle">Portal de Acceso</h1> 
+           <form>
+                 <div className= "form-group">
+                 <label>Correo Electrónico</label>
+                    <input
+                      className="form-control"
+                      type="email"
+                      name="email"
+                    />
+                </div>
+
+                <div className= "form-group">
+                 <label>Contraseña</label>
+                    <input
+                      className="form-control"
+                      type="password"
+                      name="password"
+                    />
+                </div>
+           </form>
+        </div>
         )
     }
 }
