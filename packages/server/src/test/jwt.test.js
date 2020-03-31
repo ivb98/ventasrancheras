abbdcf46b55aa38af0d8f5f222ed8ca0dc6fa459
keyner07 <skerling19@hotmail.com>
2020-03-30 21:48:21 -0400

    feat: Test for function validateToken

--- packages/server/src/test/jwt.test.js
+++ packages/server/src/test/jwt.test.js
@@ -1,14 +1,23 @@
 /* eslint-disable no-undef */
 const { expect } = require('chai');
-const { createToken } = require('../Auth/jwt/index');
+const jwt = require('../Auth/jwt/index');
 
-// eslint-disable-next-line no-undef
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6MSwiaWF0IjoxNTg1NjE2MTE3LCJleHAiOjE2NDg3MzEzMTd9.IA-RtNtkHRSC1jlo6T78SdJAz0v1TPNTVou-phJoJ38';
+
 describe('Testing the module jwt', () => {
-    it('Test for create jwt',(done) => {
-        const result = createToken(1,1);
+    it('Test for the function createToken',(done) => {
+        const result = jwt.createToken(1,1);
         expect(result)
             .to.be.a('string')
             .that.matches(/^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+/=]*$/);
+        done();
+    });
+    it('Test for the function ValidateToken',(done) => {
+        const result = jwt.validateToken(token);
+        expect(result)
+            .to.be.an('Object');
+        expect(result.id).to.be.a('Number');
+        expect(result.role).to.be.a('Number');
         done();
     })
 })
