16a5a6516f42267b05aedec58ac6c81746c698d5
keyner07 <skerling19@hotmail.com>
2020-03-31 21:21:20 -0400

    feat: Creating test delete and uppdate

--- packages/server/test/orm.test.js
+++ packages/server/test/orm.test.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
 const { expect } = require('chai');
 const faker = require('faker');
-const { createConnection, getConnectionOptions, Connection } = require("typeorm");
+const { createConnection, getConnectionOptions} = require("typeorm");
 const { SnakeNamingStrategy } = require("typeorm-naming-strategies");
-const Entity = require('../entities/EntityRepository');
+const Entity = require('../src/entities/EntityRepository');
 
 
 before(async () => {
@@ -11,14 +11,14 @@ before(async () => {
     await createConnection({ ...opts, namingStrategy: new SnakeNamingStrategy() });
 })
 
-const userTest = {
-    email: faker.internet.email('c'),
-    password: faker.internet.password(6,1,'c'),
-    name: faker.name.findName('Camilo'),
-    qboId: faker.random.number(100),
-    role: 'Employee'
-};
 describe('Test for the module orm', () => {
+    const userTest = {
+        email: faker.internet.email('c'),
+        password: faker.internet.password(6,1,'c'),
+        name: faker.name.findName('Camilo'),
+        qboId: faker.random.number(100),
+        role: 'Employee'
+    };
     it('Test for the function create', async () => {
         const result = await Entity.create(userTest, 'Employee');
         expect(result).to.be.an('Object');
@@ -33,11 +33,24 @@ describe('Test for the module orm', () => {
     });
     it('Test for the function findOne', async () => {
         const { email } = userTest;
-        const result = await Entity.findOne({ email }, 'Employee');
+        const result = await Entity.findOne({ email },'Employee');
         expect(result).to.be.an('Object');
         expect(result.email).to.be.a('String');
         expect(result.password).to.be.a('String');
         expect(result.name).to.be.a('String');
         expect(result.qboId).to.be.a('Number');
-    })
+    });
+    it('Test for the function update', async () => {
+        const emailUpdate = { email: faker.internet.email('Ivan')};
+        const { email }  = userTest;
+        const result = await Entity.update({ email }, emailUpdate, 'Employee');
+        expect(result).to.be.an('Object');
+    });
+
+    it('Test for the function delete', async () => {
+        const { name }  = userTest;
+        const result = await Entity.delete({ name }, 'Employee');
+        expect(result).to.be.an('Object');
+    });
 });
+
