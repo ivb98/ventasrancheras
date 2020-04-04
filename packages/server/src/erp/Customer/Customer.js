8a5937b5f325d90bcc7cecd10176ae6b13e97742
ivb98 <ivanvalette@hotmail.com>
2020-04-04 14:35:12 -0400

    feat: Application now loads coordinates of users
    feat: Application now loads coordinates of customers.

--- packages/server/src/erp/Customer/Customer.js
+++ packages/server/src/erp/Customer/Customer.js
@@ -9,17 +9,23 @@ module.exports.getCustomers = async () => {
     return new Promise((resolve, reject) => {
         qbo.findCustomers({ Active: true }, (err, customers) => {
             if (err) {
+                console.log(err);
                 reject(QBOUtils.parseError(err));
             } else {
                 let myCustomers = customers.QueryResponse.Customer.filter(
                     customer => !!customer.BillAddr === true
                 );
                 myCustomers = myCustomers.map(customer => {
+                    let lat = 0;
+                    let long = 0;
+                    if (customer.Notes && customer.Notes.split(",").length === 2) {
+                        [lat, long] = customer.Notes.split(",");
+                    }
                     return {
                         id: customer.Id,
                         shipAddr: {
-                            lat: 0,
-                            long: 0,
+                            lat: Number.parseFloat(lat),
+                            long: Number.parseFloat(long),
                             stringified: `${customer.BillAddr.City}, ${customer.BillAddr.CountrySubDivisionCode}, ${customer.BillAddr.Line1}`,
                         },
                         balance: customer.Balance,
